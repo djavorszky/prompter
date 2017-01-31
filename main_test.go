c867fbb1f69ed741a4358319730add2f122de525
djavorszky <daniel.javorszky@liferay.com>
2017-01-31 10:44:20 +0100

    Finish tests

--- main_test.go
+++ main_test.go
@@ -3,14 +3,16 @@ package prompter
 import (
 	"bytes"
 	"fmt"
+	"strconv"
 	"strings"
 	"testing"
 )
 
 const (
-	input   = "Nothing\n"
-	noInput = "\n"
-	defAns  = "Something"
+	input       = "Nothing\n"
+	noInput     = "\n"
+	numberInput = "1"
+	defAns      = "Something"
 )
 
 var buf bytes.Buffer
@@ -96,4 +98,146 @@ func expect(expected, actual string) (string, bool) {
 	}
 
 	return "", true
+}
+
+func TestSelectionValidInput(t *testing.T) {
+	setup(numberInput)
+
+	const q = "TestFive"
+	options := []string{"one", "two"}
+
+	var expectedOut bytes.Buffer
+
+	expectedOut.WriteString(fmt.Sprintf("%s\n", q))
+	for i, o := range options {
+		expectedOut.WriteString(fmt.Sprintf("  [%d] %s\n", i, o))
+	}
+	expectedOut.WriteString("> ")
+
+	ans, ok := AskSelection(q, options)
+	if !ok {
+		t.Error("AskSelection failed due to invalid input where valid input was provided.")
+	}
+
+	if msg, ok := expect(expectedOut.String(), buf.String()); !ok {
+		t.Error(msg)
+	}
+
+	if msg, ok := expect(numberInput, ans); !ok {
+		t.Error(msg)
+	}
+}
+
+func TestSelectionInValidInput(t *testing.T) {
+	setup(input)
+
+	const q = "TestSix"
+	options := []string{"one", "two"}
+
+	var expectedOut bytes.Buffer
+
+	expectedOut.WriteString(fmt.Sprintf("%s\n", q))
+	for i, o := range options {
+		expectedOut.WriteString(fmt.Sprintf("  [%d] %s\n", i, o))
+	}
+	expectedOut.WriteString("> ")
+	expectedOut.WriteString("Invalid input. Can only be between 0-1\n")
+
+	ans, ok := AskSelection(q, options)
+	if ok {
+		t.Error("AskSelection provided positive response even though it should have failed with invalid input.")
+	}
+
+	if msg, ok := expect(expectedOut.String(), buf.String()); !ok {
+		t.Error(msg)
+	}
+
+	if msg, ok := expect("", ans); !ok {
+		t.Error(msg)
+	}
+}
+
+func TestSelectionDefValidInput(t *testing.T) {
+	setup(numberInput)
+
+	const q, defAns = "TestSeven", 1
+	options := []string{"one", "two"}
+
+	var expectedOut bytes.Buffer
+
+	expectedOut.WriteString(fmt.Sprintf("%s (default: %d)\n", q, defAns))
+	for i, o := range options {
+		expectedOut.WriteString(fmt.Sprintf("  [%d] %s\n", i, o))
+	}
+	expectedOut.WriteString("> ")
+
+	ans, ok := AskSelectionDef(q, defAns, options)
+	if !ok {
+		t.Error("AskSelectionDef failed due to invalid input where valid input was provided.")
+	}
+
+	if msg, ok := expect(expectedOut.String(), buf.String()); !ok {
+		t.Error(msg)
+	}
+
+	if msg, ok := expect(numberInput, ans); !ok {
+		t.Error(msg)
+	}
+}
+
+func TestSelectionDefInValidInput(t *testing.T) {
+	setup(input)
+
+	const q, defAns = "TestEight", 1
+	options := []string{"one", "two"}
+
+	var expectedOut bytes.Buffer
+
+	expectedOut.WriteString(fmt.Sprintf("%s (default: %d)\n", q, defAns))
+	for i, o := range options {
+		expectedOut.WriteString(fmt.Sprintf("  [%d] %s\n", i, o))
+	}
+	expectedOut.WriteString("> ")
+	expectedOut.WriteString("Invalid input. Can only be between 0-1\n")
+
+	ans, ok := AskSelectionDef(q, defAns, options)
+	if ok {
+		t.Error("AskSelectionDef provided positive response even though it should have failed with invalid input.")
+	}
+
+	if msg, ok := expect(expectedOut.String(), buf.String()); !ok {
+		t.Error(msg)
+	}
+
+	if msg, ok := expect("", ans); !ok {
+		t.Error(msg)
+	}
+}
+
+func TestSelectionDefNoInput(t *testing.T) {
+	setup(noInput)
+
+	const q, defAns = "TestNine", 0
+	options := []string{"one", "two"}
+
+	var expectedOut bytes.Buffer
+
+	expectedOut.WriteString(fmt.Sprintf("%s (default: %d)\n", q, defAns))
+	for i, o := range options {
+		expectedOut.WriteString(fmt.Sprintf("  [%d] %s\n", i, o))
+	}
+	expectedOut.WriteString("> ")
+
+	ans, ok := AskSelectionDef(q, defAns, options)
+	if !ok {
+		t.Error("AskSelectionDef failed due to invalid input where valid input was provided.")
+	}
+
+	if msg, ok := expect(expectedOut.String(), buf.String()); !ok {
+		t.Error(msg)
+	}
+
+	if msg, ok := expect(strconv.Itoa(defAns), ans); !ok {
+		t.Error(msg)
+	}
 }
